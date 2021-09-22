50adc883dcaf102315d4e7e80269181482ec8349
s.miyazato <saya02storm3104@gmail.com>
2021-09-22 10:45:48 +0900

    監視プロパティについて

--- hello_app/src/components/HelloWorld.vue
+++ hello_app/src/components/HelloWorld.vue
@@ -3,9 +3,28 @@
     <h1>{{ title }}</h1>
     <p>{{ message }}</p>
     <hr>
-    <p>val: {{ val }}</p>
-    <div>*2： <input type="number" v-model="a" name="" id=""></div>
-    <div>^2： <input type="number" v-model="b" name="" id=""></div>
+    <div>
+      Value: <input type="number" v-model="val">
+    </div>
+    <div style="height: 10px;"></div>
+    <table>
+      <tr>
+        <th>add:</th>
+        <td>{{ add }}</td>
+      </tr>
+      <tr>
+        <th>sub:</th>
+        <td>{{ sub }}</td>
+      </tr>
+      <tr>
+        <th>multiple:</th>
+        <td>{{ mult }}</td>
+      </tr>
+      <tr>
+        <th>divide:</th>
+        <td>{{ div }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -15,33 +34,29 @@ export default {
   props: {
     title: String,
   },
-  data:function() {
+  data: function() {
     return {
-      message: 'バリデーションチェック',
+      message: '値の監視',
       val: 0,
+      add: 0,
+      sub: 0,
+      mult: 0,
+      div: 0,
     };
   },
 
-  computed: {
-    a: {
-      get: function() {
-        return this.val * 2;
-      },
-      set: function(value) {
-        return this.val = Math.floor( value / 2);
-      },
-    },
-    b: {
-      get: function() {
-        return this.val * this.val;
-      },
-      set:function(value) {
-        this.val = Math.floor(Math.sqrt(value));
-      },
-    },
+  watch: {
+    val: function(value) {
+      this.val = value;
+      let val = parseInt(value);
+      this.add = Math.floor(val + 2);
+      this.sub = Math.floor(val - 2);
+      this.mult = Math.floor(val * 2);
+      this.div = Math.floor(val / 2);
+    }
   },
   created: function() {
-    this.val = 10;
+    this.val = 5;
   },
 }
 </script>

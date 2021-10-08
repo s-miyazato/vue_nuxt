4190ba8a4532c45823f1826e425480bda9444f53
s.miyazato <saya02storm3104@gmail.com>
2021-10-08 15:54:15 +0900

    slotを使う

--- hello_app/src/App.vue
+++ hello_app/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld title="slot">
+      <p class="inner">**this is default text**</p>
+    </HelloWorld>
   </div>
 </template>
 
